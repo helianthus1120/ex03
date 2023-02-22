555d648e56f12e20a6470d6fec61666aaab459d2
helianthus1120 <16217514+helianthus1120@users.noreply.github.com>
2023-02-22 11:53:17 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -3,14 +3,10 @@ import { of, map, interval, take, from, concatAll } from 'rxjs';
 const printObserver = console.log;
 
 const firstOrderObs = of('A', 'B', 'C');
-console.log(firstOrderObs);
 // firstOrderObs.subscribe(printObserver);
 
 const highOrderObs = of(firstOrderObs, firstOrderObs, firstOrderObs);
 const flatternedObs = highOrderObs.pipe(concatAll());
-
-console.log(highOrderObs);
-console.log(flatternedObs);
 // flatternedObs.subscribe(printObserver);
 
 // const downloadUrlContent = (url) => {
